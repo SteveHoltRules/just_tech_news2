dfa253155a0a9b9c553f8941563b55adc3f51deb
Matt Luna <mat.c.luna@gmail.com>
2021-10-02 16:51:16 -0500

    feature/homepage-view

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
+const sequelize = require("./config/connection");
+
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
-
-// Path for style public journal
 app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
+app.use(require("./controllers/"));
+
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
